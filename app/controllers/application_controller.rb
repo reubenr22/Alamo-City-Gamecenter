f4c5c6f9ec2ce0f1bcbb61c69346f82afe2ec65a
Reuben <reubenr22@gmail.com>
2018-12-11 12:46:00 -0600

    beginning of slide 2
    header changes
    
    all_items changes
    -resizing images and cols
    items_by_category changes
    -first edit
    removed carrierwave thumbnails
    -resized directly through product.image.url
    added images to products and image/assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+ protect_from_forgery with: :exception
+
+  before_action :authenticate_user! 
+  before_action :categories 
+
+  def categories
+  	@categories = Category.order(:name)
+  end
 end
