6186e54571244715c36682bd043beeaea233c5ac
Reuben <reubenr22@gmail.com>
2018-12-11 13:37:46 -0600

    cart edits
    added model footer to add to cart
    cart controller
    -edited add_to_cart and view_order
    added to view_order.html
    added view cart to navbar

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,16 @@
 class CartController < ApplicationController
-  def add_to_cart
-  end
 
-  def view_order
-  end
+def add_to_cart
+  line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
+
+  line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+
+  redirect_back(fallback_location: root_path)
+end
+
+def view_order
+    @line_items = LineItem.all
+end
 
   def checkout
   end
